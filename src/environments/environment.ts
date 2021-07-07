906b540b340c5bc5ed0c01ac8a7d5a5247e54a4b
g.musolino <g.musolino@abeaform.it>
2021-07-07 08:55:43 +0200

    //

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiWeather: {
-    key: '1aacf65fc3e20898901f8b2f7d65c9f7',
+    key: '',
     urlSearch: 'http://api.openweathermap.org/data/2.5/weather?q=',
   }
 };
